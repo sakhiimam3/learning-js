49ed2ac3fc64d6ae57a56b0e24b3ddaa65555cad
sakhiimam3 <sakhiimam3411@gmail.com>
2022-10-02 19:24:00 +0500

    function and for loop

---
+++ app.js
@@ -0,0 +1,43 @@
+// var english = +prompt("Enter your english mark");
+// var urdu = +prompt("Enter your urdu mark");
+// var math = +prompt("Enter your math mark");
+
+// const result = (english, urdu, math) => {
+//   console.log("result", english);
+//   var total = english + urdu + math;
+//   alert("yours total mark is :" + total);
+//   var percentage = (total / 300) * 100;
+//   alert("yours total percentage is:" + percentage + "%");
+// };
+
+// function result2() {
+//   return result(english, urdu, math);
+// }
+
+// result2(english, urdu, math);
+
+// let array = ["english", "math", "urdu"];
+// array[0];
+// array[1];
+// array[2];
+
+// for (i = 1; i <= 10; i++) {
+//   document.write("3" + "X" + i + "=" + 3 * i + "<br>");
+// }
+
+// let array = [1, 2, 3, 44, 23, 09, 5, 9, 7];
+
+// for (i = 0; i < array.length; i++) {
+//   //console.log(array[i]);
+//   if (array[i] % 2 === 0) {
+//     console.log("here is even number", array[i]);
+//   } else {
+//     console.log("here is ood number" + array[i]);
+//   }
+// }
+
+// for (var index = 0; index < 10; index++) {
+//   console.log(index);
+// }
+
+//console.log(index);
